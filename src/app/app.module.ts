1aa8cbdb9a89b55bce542b0b3dde0548c040330b
Dorian <dorian.vanhoutte1@gmail.com>
2022-09-08 14:53:08 +0200

    co

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { BindingComponent } from './Demo/binding/binding.component';
+import { TwoWayBindingComponent } from './Demo/two-way-binding/two-way-binding.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    BindingComponent,
+    TwoWayBindingComponent,
   ],
   imports: [
     BrowserModule,
